d9430ccd75e37d5db60d56d77ce1b4312d6ba2bf
Vlad <osadchiy900@icloud.com>
2026-03-17 01:59:15 +0300

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className="dark"
+      className={cn("dark", "font-sans", fontSans.variable)}
       suppressHydrationWarning
     >
       <body
